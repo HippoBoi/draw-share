36f0c1b48a26eb98983f94b2e75c83bc84e774f7
HippoBoi <tagrifavc@gmail.com>
2024-05-30 00:16:51 -0400

    users endpoint and registering (rest framework)

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,3 +4,4 @@ class User(models.Model):
     username = models.CharField(max_length=25)
     password = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    picture = models.ImageField(upload_to="profile_picture/", blank=True, null=True)
