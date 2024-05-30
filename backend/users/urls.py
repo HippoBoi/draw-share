db8c209cec8667bf3a2644ad0d089588cba69d21
HippoBoi <tagrifavc@gmail.com>
2024-05-30 15:07:12 -0400

    login and logout endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import register_user
+from . import views
 
 urlpatterns = [
-    path("register/", register_user, name="register_user")
+    path("register/", views.register_user, name="register_user"),
+    path("login/", views.LogInView.as_view(), name="login"),
+    path("logout/", views.LogOutView.as_view(), name="logout")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 
@@ -13,3 +15,19 @@ def register_user(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogInView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({ "detail": "Logged successfully." }, status=status.HTTP_200_OK)
+        else:
+            return Response({ "error": "Could not authenticate." }, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogOutView(APIView):
+    def post(request):
+        logout(request)
+        return Response({ "detail": "Logged out successfully." }, status=status.HTTP_200_OK)
