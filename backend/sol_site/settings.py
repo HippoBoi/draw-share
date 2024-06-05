1326b1d578d878993739c122461f12848432972b
HippoBoi <tagrifavc@gmail.com>
2024-06-04 22:25:01 -0400

    storing static and media files

--- backend/sol_site/settings.py
+++ backend/sol_site/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
+    "storages",
     "users"
 ]
 
@@ -160,7 +161,19 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = "/media/"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'draw-site-bucket'
+AWS_S3_REGION_NAME = 'us-east-1'  # Por ejemplo, 'us-west-2'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Configuración de archivos estáticos
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
+# Configuración de archivos subidos por los usuarios (media)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
