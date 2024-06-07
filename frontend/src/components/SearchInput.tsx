d43dd3154296c644791e3a1915bd6236643ad290
HippoBoi <tagrifavc@gmail.com>
2024-06-06 23:14:35 -0400

    mobile UI compatibility

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -1,21 +1,17 @@
-import { Search2Icon } from '@chakra-ui/icons';
-import { IconButton, Input, InputGroup, InputLeftElement, InputRightElement, useBreakpointValue } from '@chakra-ui/react'
+import { Search2Icon, CloseIcon } from '@chakra-ui/icons';
+import { IconButton, Input, InputGroup, InputLeftElement, InputRightElement, useBreakpointValue, useColorModeValue, useOutsideClick } from '@chakra-ui/react'
+import { useRef, useState } from 'react';
 
 interface Props {
     height?: string;
-    paddingX?: string;
+    width?: string;
+    smallWidth?: string;
 }
 
-const SearchInput = ({ paddingX = "20px", height = "40px" }: Props) => {
+const SearchInput = ({ width = "250px", height = "40px", smallWidth = "100px" }: Props) => {
     const smallScreen = useBreakpointValue({ base: true, lg: false });
-    
-    if (smallScreen) {
-        return (
-            <IconButton aria-label='search'>
-                <Search2Icon />
-            </IconButton>
-        )
-    }
+    const [searchOpen, setSearchOpen] = useState(false);
+    const bgColor = useColorModeValue("red.200", "red.700");
 
     return (
         <InputGroup>
@@ -25,8 +21,18 @@ const SearchInput = ({ paddingX = "20px", height = "40px" }: Props) => {
             <Input 
                 variant={"filled"}
                 height={height}
-                paddingX={paddingX}
+                width={ smallScreen ? smallWidth : width}
                 placeholder='Buscar...' />
+            {!smallScreen && (
+                <InputRightElement>
+                    <IconButton
+                        bg={"false"}
+                        _hover={{ "bg": bgColor, "opacity": "50%" }}
+                        aria-label='close'>
+                        <CloseIcon />
+                    </IconButton>
+                </InputRightElement>
+            )}
         </InputGroup>
     )
 }
